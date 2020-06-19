01aa10d17978b2cef6d9dcd71f8cc13b91ee00cf
nahid940 <nahid940@gmail.com>
2020-06-19 16:04:38 +0600

    Comment functionality added with comment api and controller

--- app/Film.php
+++ app/Film.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Film extends Model
 {
-    //
-
     protected $fillable=['name,description,date,rating,price'];
 
     public function comments()
     {
-        return $this->hasMany(Comment::class)->orderby('id','asc');
+        return $this->hasMany(Comment::class)
+            ->orderby('id','asc');
     }
 
     public function genres()
